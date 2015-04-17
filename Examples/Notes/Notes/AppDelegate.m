21493065b3472361c280539c99e63c352e6bd292
Karl-Erik Rønsen <keronsen@gmail.com>
2015-04-17 11:03:35 +0200

    New app key for example app.

--- Examples/Notes/Notes/AppDelegate.m
+++ Examples/Notes/Notes/AppDelegate.m
@@ -5,7 +5,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [Appstax setAppKey:@"WE8wZVZQc29tSkc0"];
+    [Appstax setAppKey:@"NjE0M2Y0ODUtYTY5ZC00YmQwLTZlMTQtMWFjNTFmNzYxN2Rl"];
     return YES;
 }
 
